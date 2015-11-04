19b7a10d1a247cca6603717742a42b39b44ae0c5
jsvasani <jsvasani@users.noreply.github.com>
2015-11-03 20:18:35 -0600

    Commit initial project template

---
+++ TFSHistorySearch/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace NoName.TFSHistorySearch
+{
+    static class GuidList
+    {
+        public const string guidTFSHistorySearchPkgString = "8f3b1e9a-9b0f-456c-b9ef-b7b79e9dda7b";
+        public const string guidTFSHistorySearchCmdSetString = "9702779a-046a-47e7-825c-5813c6b9db2f";
+
+        public static readonly Guid guidTFSHistorySearchCmdSet = new Guid(guidTFSHistorySearchCmdSetString);
+    };
+}
---
+++ TFSHistorySearch/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace NoName.TFSHistorySearch
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdIdTfsSearchHistoryCommand =        0x100;
+
+
+    };
+}
